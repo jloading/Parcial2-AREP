41a2d6b584e5c7421e12661abad883b55d3a2831
ayed ayed <ayed@labinfo.is.escuelaing.edu.co>
2023-10-26 13:24:01 -0500

    first commit

---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,73 @@
+package org.example;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static spark.Spark.*;
+
+public class Server {
+
+    private static int currentIndex = -1;
+
+    private static String[] urls = {"http://localhost:4568/lucasSeq?number=", "http://localhost:4569/lucasSeq?number="};
+
+    public static void main(String... args) {
+        port(getPort());
+        staticFiles.location("/public");
+        get("lucasSeq", (req, res) -> {
+            int number = Integer.parseInt(req.queryParams("number"));
+            System.out.println("Number: " + number);
+            String url = getUrl();
+            System.out.println("URL " + url);
+            res.type("application/json");
+            String response = HttpConnection(url, number);
+            return response;
+        });
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+
+    private static String getUrl() {
+        int index = getNextNumber();
+        return urls[index];
+    }
+
+    private static int getNextNumber() {
+        currentIndex = (currentIndex + 1) % 2;
+        return currentIndex;
+    }
+
+    private static String HttpConnection(String url, int number) {
+        try {
+            URL obj = new URL(url + number);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("GET");
+            int responseCode = con.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                return response.toString();
+            } else {
+                System.out.println("GET request not worked");
+                return "{ \"error\": \"GET request not worked\" }";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "{ \"error\": \"Internal server error\" }";
+        }
+    }
+}
---
+++ src/main/java/org/example/SparkWebServer.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import static spark.Spark.*;
+
+public class SparkWebServer {
+
+
+    public static void main(String... args){
+        port(getPort());
+        get("lucasSeq", (req,res) -> {
+
+            res.type("text/json");
+
+            int number = Integer.parseInt(req.queryParams("number"));
+
+            return lucasSeq(number);
+        });
+    }
+
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4568;
+    }
+
+    public static String lucasSeq(int number){
+        String result = "";
+        String response = "";
+        int first = 2;
+        int second = 1;
+        int temp = 0;
+        int []arr = new int[number];
+
+        if(number == 1)
+        {
+            result = "2";
+            return result;
+        }
+
+        else if(number == 2)
+        {
+            result = "1";
+            return result;
+        }
+        arr[0] = 2;
+        arr[1] = 1;
+        for(int i = 2; i < number; i++)
+        {
+            temp = first + second;
+            arr[i] = temp;
+            first = second;
+            second = temp;
+        }
+        for(int i = 0; i < number; i++)
+        {
+            result += arr[i]+",";
+
+        }
+
+        response = "{\n" +
+                "\n" +
+                " \"operation\": \"Secuencia de Lucas\",\n" +
+                "\n" +
+                " \"input\": " + String.valueOf(number) + ",\n" +
+                "\n" +
+                " \"output\": " + result + "\n" +
+                "\n" +
+                "}";
+
+        return response;
+    }
+
+}
